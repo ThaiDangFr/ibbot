ea11d4a6b9a677a1e8782cdad20bec8b3bbd6308
Thai Dang <thai.dang.fr@gmail.com>
2019-02-07 17:42:08 +0100

    initial import

---
+++ ibbot.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+require 'mechanize'
+require 'logger'
+
+p123id=ARGV[0]
+
+if p123id.nil?
+  raise "Syntax : #{__FILE__} <portfolio id>"
+end
+
+
+loginurl = "https://www.portfolio123.com/login.jsp?url=%2F"
+pfurl = "https://www.portfolio123.com/p123/DownloadPortHoldings?portid=#{p123id}"
+username = ENV['P123USR']
+password = ENV['P123PWD']
+
+agent = Mechanize.new
+agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+agent.user_agent_alias = 'Linux Firefox'
+agent.log = Logger.new(STDERR)
+page = agent.get(loginurl)
+form = page.forms.first
+
+
+form["LoginUsername"] = username
+form["LoginPassword"] = password
+form["url"] = "/"
+agent.submit(form, form.buttons.first)
+page = agent.get(pfurl)
+
+pp page.content.split("\n")
+
+
+
