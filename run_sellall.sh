d9a4be50938088a1481c1a879ec7d2bb305a9ead
Thai Dang <thai.dang.fr@gmail.com>
2019-04-02 16:51:16 +0200

    in dev

---
+++ run_sellall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+# Customize here the main script with your parameters
+
+export scriptPath=$(dirname $0)
+cd ${scriptPath}
+
+if [ $(date +%w) -gt 5 ] ||  [ $(date +%w) -lt 1 ]; then
+    exit 0
+fi
+
+
+LOGFILE=sellall.log
+./ibbot.rb -v --username $P123USR --password $P123PWD --import_pf PERSO -v #--logfile $LOGFILE --rebalance --commit
+
+#if [ $? -ne 0 ];then
+#    ./report.rb --email $P123MAIL --subject "IBBOT failed | $LOGFILE" --logfile $LOGFILE
+#fi
