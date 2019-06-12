7954faa5f254f56ad5e07e5bba362dd86dbab859
ThaiDang <thai.dang.fr@gmail.com>
2019-06-12 14:12:36 +0200

    update strategy

--- run_pro.sh
+++ run_pro.sh
@@ -9,7 +9,7 @@ if [ $(date +%w) -gt 5 ] ||  [ $(date +%w) -lt 1 ]; then
 fi
 
 LOGFILE=pro.log
-./ibbot.rb -v --username $P123USR --password $P123PWD --import_pf PRO --import_sim 1557921:98 --logfile $LOGFILE --rebalance --commit
+./ibbot.rb -v --username $P123USR --password $P123PWD --import_pf PRO --import_sim 1559123:10 --import_sim 1575679:88 --logfile $LOGFILE --rebalance --commit
 
 if [ $? -ne 0 ];then
     ./report.rb --email $P123MAIL --subject "IBBOT failed | $LOGFILE" --logfile $LOGFILE
